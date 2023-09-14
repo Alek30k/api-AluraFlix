73c2778c644f25be013d8950fcb3a376532f0080
Alek30k <alemank23k@gmail.com>
2023-09-14 18:18:12 -0300

    last commit

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const connect = () => {
   mongoose
     .connect(process.env.MONGO)
     .then(() => {
-      console.log("Connected to DB");
+      console.log(
+        "mongodb+srv://Alek30k:aleisa562196@cluster0.xxdbz30.mongodb.net/alauraflix?retryWrites=true&w=majority"
+      );
     })
     .catch((err) => {
       throw err;
